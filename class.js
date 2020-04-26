06cd5b33a04ab98673645445dc1a72f3db59c522
otakubarbudinho <64333806+otakubarbudinho@users.noreply.github.com>
2020-04-26 16:21:58 -0300

    Update class.js

--- class.js
+++ class.js
@@ -1,23 +1,22 @@
 //** Trabalhando com Classes no JavaScript */
-
-class clientes {
-    constructor(nome, cpf) {
-
+class pedidos {
+    constructor(qtd) {
+        this.qtd = qtd;
+    }
+    quantidadePedidos() {
+        console.log('Total de ' + this.qtd + ' pedidos')
+    }
+}
+class clientes extends pedidos {
+    constructor(nome, cpf, qtd) {
+        super(qtd);
         this.nome = nome;
-
         this.cpf = cpf;
     }
-
     exibirNome() {
         console.log('O nome do cliente é' + this.nome);
-
     }
-
 }
-
-const cliente = new clientes('Rodrigo', '555.348.440-51');
-
-
-
-
-console.log(cliente.exibirNome());
+const cliente = new clientes('Rodrigo', '555.348.440-51', '100');
+cliente.exibirNome();
+cliente.quantidadePedidos();
